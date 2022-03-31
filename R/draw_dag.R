7ed4e774ee9879453d73dd8079800306d35649f3
emilio-berti <emilio.berti90@gmail.com>
2022-03-31 14:26:41 +0200

    draw dag

---
+++ R/draw_dag.R
@@ -0,0 +1,33 @@
+#' @title Draw causal graph (DAG)
+#'
+#' @param d data.frame, with columns 'from' and 'to'.
+#'
+#' @details An arrow is drawn between column 'from' and 'to', indicating
+#' the potential causal relationship among variables.
+#'
+#' @return
+#'
+#' @example
+#' \donttest{
+d <- data.frame(
+  from = c("Rain", "Sun", "Latitude", "Latitude"),
+  to = c("Mental health", "Mental health", "Rain", "Sun")
+)
+#' draw_dag(d)
+#' }
+draw_dag <- function(d, draw = FALSE) {
+  d$from <- gsub(" ", "_", d$from)
+  d$to <- gsub(" ", "_", d$to)
+  # redirect sink to temporary file
+  tmp <- tempfile()
+  sink(tmp)
+  g <- '
+    cat("digraph {\n graph []\n node [shape = plaintext]\n edge []\n")
+    for (i in seq_len(nrow(d))) cat(d$from[i], "->", d$to[i], "\n")
+    cat("}\n")
+  '
+  eval(parse(text = g)) #write to tempfile
+  sink() #restore default sink
+  dag <- paste0(readLines(tmp), collapse = "\n")
+  if (draw) DiagrammeR::grViz
+}
